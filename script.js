c4fa9a680fc9512161411757f2b64418e405d274
Angel Jesus Anchevida Uco <angelanchevida@gmail.com>
2022-01-01 22:35:49 -0600

    CRUD

---
+++ script.js
@@ -0,0 +1,92 @@
+var app = new function()
+{
+    this.el = document.getElementById('games');
+
+    this.games = [];
+
+    this.FetchAll = function()
+    {
+        var data = '';
+        if(this.games.length > 0)
+        {
+            for(i = 0; i < this.games.length; i++)
+            {
+                data += '<tr>';
+                data += '<td>'+(i+1)+ ". " + this.games[i] + '</td>';
+                data += '<td><button onclick="app.Edit(' + i + ')" class="btn btn-warning">Edit</button></td>';
+                data += '<td><button onclick="app.Delete(' + i + ')" class="btn btn-danger">Delete</button></td>';
+                data += '</tr>';
+            }
+        }
+        this.Count(this.games.length);
+        return this.el.innerHTML = data;
+    };
+    
+    this.Add = function()
+    {
+        el = document.getElementById('add-juego');
+        var game = el.value;
+
+        if(game)
+        {
+            this.games.push(game.trim())
+            el.value = '';
+            this.FetchAll();
+        }
+    };
+
+    this.Edit = function(item)
+    {
+        var el = document.getElementById('edit-juego');
+        el.value = this.games[item];
+        document.getElementById('edit-box').style.display = 'block';
+        self = this;
+
+        document.getElementById('save-edit').onsubmit = function()
+        {
+            var game = el.value;
+
+            if(game)
+            {
+                self.games.splice(item, 1, game.trim());
+
+                self.FetchAll();
+
+                CloseInput();
+            }
+        }
+    };
+    
+    this.Delete = function(item)
+    {
+        this.games.splice(item, 1);
+        this.FetchAll();
+    };
+
+    this.Count = function(data)
+    {
+        var el = document.getElementById('counter')
+        var name = 'Videojuegos';
+
+        if(data)
+        {
+            if(data == 1)
+            {
+                name = 'Videojuego';
+            }
+            el.innerHTML = data + ' ' + name;
+        }
+        else
+        {
+            el.innerHTML = 'No. ' + name;
+        }
+    };
+}
+
+
+  app.FetchAll();
+
+  function CloseInput()
+  {
+      document.getElementById('edit-box').style.display = 'none';
+  }
